cdc4561546bf86c80084079543a13a0489bbc47c
Xi-yu <510993715@qq.com>
2023-04-21 08:50:35 +0800

    [226] 翻转二叉树

---
+++ src/226.翻转二叉树.ts
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=typescript
+ *
+ * [226] 翻转二叉树
+ *
+ * https://leetcode.cn/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (79.49%)
+ * Likes:    1563
+ * Dislikes: 0
+ * Total Accepted:    653.4K
+ * Total Submissions: 821.9K
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ *
+ * 输入：root = [4,2,7,1,3,6,9]
+ * 输出：[4,7,2,9,6,3,1]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ *
+ *
+ * 输入：root = [2,1,3]
+ * 输出：[2,3,1]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = []
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数目范围在 [0, 100] 内
+ * -100 <= Node.val <= 100
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+// 递归
+// 时间: O(n)
+// 空间: O(n)
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (!root) {
+    return null;
+  }
+  const left = invertTree(root.left);
+  const right = invertTree(root.right);
+  root.left = right;
+  root.right = left;
+  return root;
+}
+// @lc code=end
