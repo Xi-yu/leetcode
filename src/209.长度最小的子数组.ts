1d9035c239db6c6c957e51048b0d4f2383eb6181
Xi-yu <510993715@qq.com>
2023-05-14 15:49:22 +0800

    [209] 长度最小的子数组

--- src/209.长度最小的子数组.ts
+++ src/209.长度最小的子数组.ts
@@ -63,7 +63,59 @@
  */
 
 // @lc code=start
+// 前缀和+二分查找
+// 时间: O(nlogn)
+// 空间: O(n)
+function minSubArrayLen1(target: number, nums: number[]): number {
+  const length: number = nums.length;
+  // 前缀和
+  const sums: number[] = new Array(length).fill(0);
+  sums[0] = nums[0];
+  for (let i = 1; i < length; i++) {
+    sums[i] = sums[i - 1] + nums[i];
+  }
+  let ans: number = 0;
+  for (let i = 0; i < length; i++) {
+    // 二分查找
+    let left = i,
+      right = length - 1;
+    while (left <= right) {
+      const mid = left + ((right - left) >>> 1);
+      const cur = sums[mid] - (i > 0 ? sums[i - 1] : 0);
+      if (cur >= target) {
+        // 更新答案，继续查找[left,mid-1]
+        ans = ans === 0 ? mid - i + 1 : Math.min(ans, mid - i + 1);
+        right = mid - 1;
+      } else {
+        // 继续查找[mid+1,right]
+        left = mid + 1;
+      }
+    }
+  }
+  return ans;
+}
+
+// 滚动数组
+// 时间: O(n)
+// 空间: O(1)
 function minSubArrayLen(target: number, nums: number[]): number {
-  // todo
+  const length: number = nums.length;
+  let ans: number = 0;
+  let sum: number = nums[0]; // [0,0]的和
+  // 滑动窗口（双指针）
+  for (let i = 0, j = 0; i < length && j < length; ) {
+    if (sum >= target) {
+      // 如果[i,j]的和大于等于target，先更新答案
+      ans = ans === 0 ? j - i + 1 : Math.min(ans, j - i + 1);
+      // 然后右移左指针，缩小滑动窗口的大小，继续判断
+      sum -= nums[i];
+      i++;
+    } else {
+      // 否则，右移右指针，增大滑动窗口的大小，继续判断
+      j++;
+      sum += nums[j];
+    }
+  }
+  return ans;
 }
 // @lc code=end
