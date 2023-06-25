cec1928e37cbb3f34508760dc9aee2f18a19126f
Xi-yu <510993715@qq.com>
2023-06-25 09:47:25 +0800

    [9] 回文数

---
+++ src/9.回文数.ts
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=typescript
+ *
+ * [9] 回文数
+ *
+ * https://leetcode.cn/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (56.09%)
+ * Likes:    2571
+ * Dislikes: 0
+ * Total Accepted:    1.4M
+ * Total Submissions: 2.5M
+ * Testcase Example:  '121'
+ *
+ * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+ *
+ * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ *
+ *
+ * 例如，121 是回文，而 123 不是。
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：x = 121
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：x = -121
+ * 输出：false
+ * 解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：x = 10
+ * 输出：false
+ * 解释：从右向左读, 为 01 。因此它不是一个回文数。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * -2^31 <= x <= 2^31 - 1
+ *
+ *
+ *
+ *
+ * 进阶：你能不将整数转为字符串来解决这个问题吗？
+ *
+ */
+
+// @lc code=start
+// 数字翻转后比较
+// 时间: O(logx)
+// 空间: O(1)
+function isPalindrome(x: number): boolean {
+  if (x < 0) {
+    return false;
+  }
+  if (x >= 0 && x <= 9) {
+    return true;
+  }
+  let ans = 0;
+  let cur = x;
+  while (cur > 0) {
+    ans = ans * 10 + (cur % 10);
+    cur = Math.floor(cur / 10);
+  }
+  return ans === x;
+}
+// @lc code=end
