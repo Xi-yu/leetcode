35b94aad6581b6b87d8bd108ceecd00a973b96cf
Xi-yu <510993715@qq.com>
2023-08-06 21:00:35 +0800

    [611] 有效三角形的个数

--- src/611.有效三角形的个数.ts
+++ src/611.有效三角形的个数.ts
@@ -49,7 +49,7 @@
 // 排序+暴力遍历
 // 时间: O(nlogn+n^3)
 // 空间: O(logn)
-function triangleNumber(nums: number[]): number {
+function triangleNumber1(nums: number[]): number {
   // 先排序
   nums.sort((a, b) => a - b);
 
@@ -67,6 +67,78 @@ function triangleNumber(nums: number[]): number {
           // 否则，不是有效的三角形，并且后面的也不用再判断了，因为排过序了
           break;
         }
+      }
+    }
+  }
+
+  return ans;
+}
+
+// 排序+二分遍历
+// 时间: O(nlogn+n^2logn)
+// 空间: O(logn)
+function triangleNumber2(nums: number[]): number {
+  // 先排序
+  nums.sort((a, b) => a - b);
+
+  let ans = 0;
+  const len = nums.length;
+
+  for (let i = 0; i < len - 2; i++) {
+    for (let j = i + 1; j < len - 1; j++) {
+      const sum = nums[i] + nums[j];
+      // 因为排了序了，可以二分查找，找到最后一个小于sum的下标
+      let left = j + 1,
+        right = len - 1;
+      let k = j;
+      while (left <= right) {
+        const mid = left + ((right - left) >> 1);
+        if (nums[mid] < sum) {
+          k = mid;
+          left = mid + 1;
+        } else {
+          right = mid - 1;
+        }
+      }
+      ans += k - j;
+    }
+  }
+
+  return ans;
+}
+
+// 排序+双指针
+// 时间: O(nlogn+)
+// 空间: O(logn)
+function triangleNumber(nums: number[]): number {
+  const len = nums.length;
+  // 先排序
+  nums.sort((a, b) => a - b);
+
+  let ans = 0;
+
+  for (let i = 0; i < len - 2; i++) {
+    // nums[i]：第一条边
+    // nums[j]：第二条边
+    let j = i + 1;
+    // k是最后一个小于nums[i]+nums[j]的下标
+    let k = j; // 因为j+1对应的k一定大于等于j对应的k，所以j变化时，k不用从j+1开始遍历
+    while (j < len - 1) {
+      while (k + 1 < len) {
+        // nums[k+1]：第三条边
+        if (nums[k + 1] < nums[i] + nums[j]) {
+          // 这三条边是一个有效的三角形
+          k++;
+        } else {
+          // 否则，不是有效的三角形，第二条边换j+1来遍历
+          break;
+        }
+      }
+      ans += k - j;
+      j++;
+      if (k < j) {
+        // 特殊情况：保证k>=j，这样k-j才不会出现负数
+        k = j;
       }
     }
   }
