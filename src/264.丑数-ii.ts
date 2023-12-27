a749667ae0ed0c26883a6f3b2ebb17e80118ed40
Xi-yu <510993715@qq.com>
2023-12-27 15:37:36 +0800

    [264] 丑数 II

--- src/264.丑数-ii.ts
+++ src/264.丑数-ii.ts
@@ -55,19 +55,17 @@ function nthUglyNumber1(n: number): number {
   const minHeap = new MinHeap();
   // 初始化把最小的丑数放进去
   minHeap.push(1);
-  hash.add(1);
   let ans: number = 1;
 
-  for (let i = 0; i < n; i++) {
-    const min = minHeap.pop();
-    // 可能存在重复，所以需要一个hash表记录判断一下
+  for (let i = 1; i <= n; i++) {
+    ans = minHeap.pop();
     for (const factor of primeFactors) {
-      if (!hash.has(min * factor)) {
-        minHeap.push(min * factor);
-        hash.add(min * factor);
+      // 可能存在重复，所以需要一个hash表记录判断一下
+      if (!hash.has(factor * ans)) {
+        minHeap.push(factor * ans);
+        hash.add(ans * factor);
       }
     }
-    ans = min;
   }
 
   return ans;
@@ -114,7 +112,7 @@ class MinHeap {
     let i = startIndex;
     while (i > 0) {
       const parentIndex = this.getParentIndex(i);
-      if (this.heap[i] < this.heap[parentIndex]) {
+      if (parentIndex >= 0 && this.heap[parentIndex] > this.heap[i]) {
         this.swap(i, parentIndex);
         i = parentIndex;
       } else {
@@ -130,21 +128,18 @@ class MinHeap {
       let swapIndex = i;
       if (
         leftIndex < this.heap.length &&
-        this.heap[swapIndex] > this.heap[leftIndex]
+        this.heap[leftIndex] < this.heap[swapIndex]
       ) {
         swapIndex = leftIndex;
       }
       if (
         rightIndex < this.heap.length &&
-        this.heap[swapIndex] > this.heap[rightIndex]
+        this.heap[rightIndex] < this.heap[swapIndex]
       ) {
         swapIndex = rightIndex;
       }
       if (swapIndex !== i) {
-        [this.heap[swapIndex], this.heap[i]] = [
-          this.heap[i],
-          this.heap[swapIndex],
-        ];
+        this.swap(swapIndex, i);
         i = swapIndex;
       } else {
         break;
@@ -154,17 +149,42 @@ class MinHeap {
 }
 
 // dp
-// 时间: O()
-// 空间: O()
+// 时间: O(n)
+// 空间: O(n)
 function nthUglyNumber(n: number): number {
-  const primeFactors = [2, 3, 5];
   // dp[i]: 第i+1个丑数
   const dp: number[] = new Array(n).fill(1);
+  // 第1个丑数是1
+  dp[0] = 1;
+  // p2: 乘2的指针
+  let p2 = 0;
+  // p3: 乘3的指针
+  let p3 = 0;
+  // p5: 乘5的指针
+  let p5 = 0;
 
-  // 第一个丑数是1, 从第二个开始计算
-  for (let i = 1; i < n; i++) {}
+  for (let i = 1; i < n; i++) {
+    // 当前位置i的丑数一定是位置小于i的某一个丑数乘2或3或5得到的
+    const next2 = dp[p2] * 2;
+    const next3 = dp[p3] * 3;
+    const next5 = dp[p5] * 5;
+    dp[i] = Math.min(next2, next3, next5);
+    if (dp[i] === next2) {
+      p2++;
+    }
+    if (dp[i] === next3) {
+      p3++;
+    }
+    if (dp[i] === next5) {
+      p5++;
+    }
+  }
 
   return dp[n - 1];
 }
+// [1,2,3,4,5,6,8,9,10,12,15,16,18,20,24,25,27,30,32,36,40,45,48,50,54]
+// 54
+// 54
+// 60
 // @lc code=end
 // [1,2,3,4,5,6,8,9,10,12,15,16,18,20,24,25,27,30,32,36,40,45,48,50,54]
